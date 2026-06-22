a936e1de2383c1624b67f52f9786f9a9e8cf877e
Aleksandr Belousov <Sasha1112200196@gmail.com>
2026-06-22 20:37:41 +0800

    Adding a comment with the way the app is built via "nuitka"

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ from MyApp_class import MyApp
 if __name__ == "__main__":
     MyApp().run()
 
+# pip install pyinstaller
 # pyinstaller --noconsole --add-data ".\\icon_of_weather\\png;icon_of_weather\\png" --icon=.\\icon_of_weather\\logo.ico --name=averageweather --onefile main.py
+
+# pip install nuitka
+# python -m nuitka --standalone --onefile --windows-disable-console --include-data-dir="icon_of_weather/png=icon_of_weather/png" --windows-icon-from-ico="icon_of_weather/logo.ico" --output-filename=averageweather main.py
